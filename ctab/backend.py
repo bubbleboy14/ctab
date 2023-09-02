070ca7b215a4aa57c43d4cc970e72426e0a4149f
bubbleboy14 <mario.balibrera@gmail.com>
2023-09-02 03:58:12 -0700

    backend: halfway-working dydxtest() and other partial dydx stuff

--- ctab/backend.py
+++ ctab/backend.py
@@ -1,10 +1,11 @@
 import rel, json, websocket
 
-try:
-	from cantools.util import log
-except:
-	def log(msg):
-		print(msg)
+from dydx3 import Client
+from dydx3.helpers.request_helpers import generate_now_iso
+from dydx3.constants import API_HOST_GOERLI, NETWORK_ID_GOERLI, WS_HOST_GOERLI
+
+def log(*msg):
+	print(*msg)
 
 LOUD = True
 
@@ -12,6 +13,28 @@ def crsub(streamname):
 	return {
 		"name": "SubscribeTicker",
 		"data": streamname
+	}
+
+def ddsub(streamname):
+	print("opened - sending sub block")
+	now = generate_now_iso()
+	dclient = Client(
+		network_id=NETWORK_ID_GOERLI,
+		host=API_HOST_GOERLI
+	)
+	sig = dclient.private.sign(
+		request_path='/ws/accounts',
+		method='GET',
+		iso_timestamp=now,
+		data={}
+	)
+	return {
+		"type": "subscribe",
+		"channel": "v3_accounts",
+		"accountNumber": '0',
+		"market": streamname,
+		"timestamp": now,
+		"signature": sig
 	}
 
 def subber(streamname, submaker, doafter=None):
@@ -28,6 +51,10 @@ def jsend(ws):
 	return _jsend
 
 platforms = {
+	"dydx": {
+		"feed": WS_HOST_GOERLI,
+		"subber": ddsub
+	},
 	"chainrift": {
 		"feed": "wss://ws.chainrift.com/v1",
 		"subber": crsub
@@ -39,10 +66,10 @@ platforms = {
 
 def feed(platname, streamname, **cbs): # {on_message,on_error,on_open,on_close}
 	plat = platforms[platname]
-	feed = getattr(plat, "feed", plat["feeder"](streamname,))
+	feed = "feed" in plat and plat["feed"] or plat["feeder"](streamname)
 	if "subber" in plat:
 		cbs["on_open"] = subber(streamname,
-			plat["subber"], getattr(cbs, "on_open"))
+			plat["subber"], getattr(cbs, "on_open", None))
 	ws = websocket.WebSocketApp(feed, **cbs)
 	ws.jsend = jsend(ws)
 	ws.run_forever(dispatcher=rel)
@@ -51,7 +78,12 @@ def feed(platname, streamname, **cbs): # {on_message,on_error,on_open,on_close}
 def echofeed(platform="gemini", streamname="ETHBTC"):
 	return feed(platform, streamname,
 		on_message = lambda ws, msg : log(msg),
-		on_error = lambda *msg : log("error!", *msg))
+		on_close = lambda ws, code, msg: log("close!", code, msg),
+		on_error = lambda ws, exc : log("error!", str(exc)))
+
+def dydxtest():
+	echofeed("dydx", "ETH-BTC")
+	start()
 
 def events(message):
 	return json.loads(message)["events"]
