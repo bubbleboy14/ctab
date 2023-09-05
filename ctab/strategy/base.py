01ce961948d709a20ff1e93d31c61b4d99f98b2f
bubbleboy14 <mario.balibrera@gmail.com>
2023-09-05 04:52:11 -0700

    strategy, Base.process(): pass symbol to compare()

--- ctab/strategy/base.py
+++ ctab/strategy/base.py
@@ -10,5 +10,5 @@ class Base(object):
 		print("Strategist[%s:%s] %s"%(self.__class__.__name__,
 			self.symbol, " ".join([str(m) for m in msg])))
 
-	def process(self, event, history):
-		self.compare(event["side"], float(event["price"]), event, history)
+	def process(self, symbol, event, history):
+		self.compare(symbol, event["side"], float(event["price"]), event, history)
