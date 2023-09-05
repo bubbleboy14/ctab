9ebe0a9a5217cdaa1d4088333fcdee28d68c9c6c
bubbleboy14 <mario.balibrera@gmail.com>
2023-09-05 06:50:40 -0700

    basic (skeletal) trader

--- ctab/trader.py
+++ ctab/trader.py
@@ -1,4 +1,22 @@
 
 class Trader(object):
 	def __init__(self):
-		pass # should know how to trade
+		self.recommendations = []
+
+	def log(self, *msg):
+		print("Trader %s"%(" ".join([str(m) for m in msg]),))
+
+	def recommend(self, recommendation):
+		self.recommendations.append(recommendation)
+
+	def shouldTrade(self, recommendation):
+		pass
+
+	def trade(self, recommendation):
+		pass
+
+	def tick(self):
+		# first rank in terms of payout
+		for recommendation in self.recommendations:
+			self.shouldTrade(recommendation) and self.trade(recommendation)
+		self.recommendations = []
