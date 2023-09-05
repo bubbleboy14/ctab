2fbf5a5c22a38c0d79988fe6e6fc0439d980f7f4
bubbleboy14 <mario.balibrera@gmail.com>
2023-09-05 10:20:43 -0700

    Office supports (in tick() and constructor) centralized trader; added trader True to default (dydx BTC-USD/ETH-USD slosh) Office run

--- ctab/office.py
+++ ctab/office.py
@@ -1,15 +1,19 @@
 from backend import log, rel, start
 from strategist import strategies
 from manager import Manager
+from trader import Trader
 
 class Office(object):
-	def __init__(self, platform, symbols, strategist=None):
+	def __init__(self, platform, symbols, strategist=None, trader=False):
 		self.platform = platform
 		self.symbols = symbols
-		self.strategist = strategist and strategies[strategist](symbols)
+		self.trader = trader and Trader()
+		self.strategist = strategist and strategies[strategist](symbols,
+			self.trader and self.trader.recommend)
 		self.managers = {}
 		for symbol in symbols:
-			self.managers[symbol] = Manager(platform, symbol, self.strategist)
+			self.managers[symbol] = Manager(platform,
+				symbol, self.strategist, self.trader)
 		self.log("initialized %s managers"%(len(symbols),))
 		rel.timeout(1, self.tick)
 
@@ -17,15 +21,19 @@ class Office(object):
 		log("Office[%s] %s"%(self.platform, " ".join(msg)))
 
 	def tick(self):
+		manStrat = manTrad = True
 		if self.strategist:
 			self.strategist.tick()
-		else:
-			for manager in self.managers:
-				self.managers[manager].tick()
+			manStrat = False
+		if self.trader:
+			self.trader.tick()
+			manTrad = False
+		for manager in self.managers:
+			self.managers[manager].tick(manStrat, manTrad)
 		return True
 
 if __name__ == "__main__":
-	Office("dydx", ["BTC-USD", "ETH-USD"], "slosh")
+	Office("dydx", ["BTC-USD", "ETH-USD"], "slosh", True)
 #	Office("dydx", ["BTC-USD"])
 #	Office("gemini", ["BTCUSD", "ETHUSD", "ETHBTC"])
 	start()
