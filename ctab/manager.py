e326c3a03a2d52f3fa9ae76c347246a3b612256a
bubbleboy14 <mario.balibrera@gmail.com>
2023-09-05 10:17:15 -0700

    Manager: constructor supports trader (default None) kwarg, builds own Trader if necessary, and passes trader.recommend() to strategist.setRecommender(); tick() supports strat/trad (default True) kwargs

--- ctab/manager.py
+++ ctab/manager.py
@@ -1,20 +1,23 @@
 from backend import log
-from observer import Observer
 from strategist import strategies
+from observer import Observer
+from trader import Trader
 
 class Manager(object):
-	def __init__(self, platform, symbol, strategist=None, strategy="rsi"):
+	def __init__(self, platform, symbol, strategist=None, trader=None, strategy="rsi"):
 		self.platform = platform
 		self.symbol = symbol
+		self.trader = trader or Trader()
 		self.observer = Observer(platform, symbol, self.observe)
 		self.strategist = strategist or strategies[strategy](symbol)
-		# TODO: trader
+		self.strategist.setRecommender(trader.recommend)
 
 	def log(self, *msg):
 		log("Manager[%s:%s] %s"%(self.platform, self.symbol, " ".join(msg)))
 
-	def tick(self):
-		self.strategist.tick(self.observer.history)
+	def tick(self, strat=True, trad=True):
+		strat and self.strategist.tick(self.observer.history)
+		trad and self.trader.tick()
 
 	def observe(self, event):
 		self.strategist.process(self.symbol, event, self.observer.history)
