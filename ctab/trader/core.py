c1c1b5ab6c3505950c3cd158ff372ed7cddf43a1
bubbleboy14 <mario.balibrera@gmail.com>
2023-09-14 00:28:54 -0700

    Trader: trades[] and live flag; note() (basic); trade() notes() recommendation{}

--- ctab/trader/core.py
+++ ctab/trader/core.py
@@ -1,10 +1,16 @@
 
 class Trader(object):
-	def __init__(self):
+	def __init__(self, live=False):
 		self.recommendations = []
+		self.live = live
+		self.trades = []
 
 	def log(self, *msg):
 		print("\nTrader %s\n"%(" ".join([str(m) for m in msg]),))
+
+	def note(self, recommendation):
+		# TODO: wrap in timestamped object...?
+		self.trades.append(recommendation)
 
 	def recommend(self, recommendation):
 		self.log("recommended:", recommendation)
@@ -15,9 +21,13 @@ class Trader(object):
 
 	def trade(self, recommendation):
 		self.log("\n\n\n", "TRADING", recommendation, "\n\n\n")
+		self.note(recommendation)
+		if self.live:
+			pass # do the trade via Agent
 
 	def tick(self):
 		# first rank in terms of payout
 		for recommendation in self.recommendations:
 			self.shouldTrade(recommendation) and self.trade(recommendation)
 		self.recommendations = []
+		# TODO: review trades[] (track wins/losses)
