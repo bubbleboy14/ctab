f3cd1af835c07d2cadcb7653b315456cb50b1cca
bubbleboy14 <mario.balibrera@gmail.com>
2023-09-14 01:13:30 -0700

    added agent import to loader

--- ctab/__init__.py
+++ ctab/__init__.py
@@ -2,6 +2,7 @@ from . import init
 from backend import feed
 from observer import Observer
 from trader import Trader
+from agent import Agent
 from strategist import strategies
 from manager import Manager
 from office import Office
