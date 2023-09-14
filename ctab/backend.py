faf74463fd3be7fe5a61798f98679a7bc5cb34df
bubbleboy14 <mario.balibrera@gmail.com>
2023-09-14 02:00:27 -0700

    backend: read() and write(); membank{}, remember(), and recall()

--- ctab/backend.py
+++ ctab/backend.py
@@ -1,7 +1,31 @@
-import rel, json, websocket
+import os, base64, rel, json, websocket
 
 def log(*msg):
 	print(*msg)
+
+def read(fname):
+	if not os.path.exists(fname):
+		return
+	f = open(fname, 'r')
+	data = f.read()
+	f.close()
+	return data and json.loads(base64.b64decode(data).decode())
+
+def write(fname, data):
+	f = open(fname, 'w')
+	f.write(base64.b64encode(json.dumps(data).encode()).decode())
+	f.close()
+
+membank = {}
+remembered = read(".membank")
+remembered and membank.update(remembered)
+
+def remember(key, data):
+	membank[key] = data
+	write(".membank", membank)
+
+def recall(key):
+	return membank.get(key, None)
 
 _predefs = {
 	"strategy": "rsi",
