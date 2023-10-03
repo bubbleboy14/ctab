79f2919f09e883a73e463d7009f4f045f7cf4caa
bubbleboy14 <mario.balibrera@gmail.com>
2023-10-02 23:00:36 -0700

    Office: assess() and review(); constructor passes review() to each Manager; tick() calls review() if global trader

--- ctab/office.py
+++ ctab/office.py
@@ -13,13 +13,52 @@ class Office(object):
 		self.strategist = globalStrategy and strat(symbols, trec)
 		self.managers = {}
 		for symbol in symbols:
-			self.managers[symbol] = Manager(platform, symbol,
+			self.managers[symbol] = Manager(platform, symbol, self.review,
 				self.strategist or strat(symbol, trec), self.trader)
 		self.log("initialized %s managers"%(len(symbols),))
 		rel.timeout(1, self.tick)
 
 	def log(self, *msg):
 		log("Office[%s] %s"%(self.platform, " ".join(msg)))
+
+	def assess(self, trade, curprice=None):
+		action = trade["action"]
+		price = trade["price"]
+		symbol = trade["symbol"]
+		curprice = curprice or self.managers[symbol].latest["price"]
+		if not curprice: # can this even happen?
+			return self.log("skipping assessment (waiting for %s price)"%(symbol,))
+		if action == "BUY":
+			isgood = curprice > price
+		else: # SELL
+			isgood = curprice < price
+		self.log("%s %s at %s - %s trade!"%(action,
+			symbol, price, isgood and "GOOD" or "BAD"))
+
+	def review(self, symbol=None, trader=None, curprice=None):
+		mans = self.managers
+		if symbol:
+			man = mans[symbol]
+			trader = man.trader
+			curprice = man.latest["price"]
+		else:
+			trader = trader or self.trader
+		tz = trader.trades
+		if symbol:
+			tz = list(filter(lambda t : t["symbol"] == symbol, tz))
+		if not tz:
+			return self.log("skipping review (waiting for trades)")
+		lstr = ["review %s"%(len(tz),)]
+		symbol and lstr.append(symbol)
+		lstr.append("trades - current")
+		if curprice:
+			lstr.append("price is %s"%(curprice,))
+		else:
+			lstr.append("prices are:")
+			lstr.append("; ".join(["%s at %s"%(sym, mans[sym].latest["price"]) for sym in mans.keys()]))
+		self.log(*lstr)
+		for trade in tz:
+			self.assess(trade, curprice)
 
 	def tick(self):
 		manStrat = manTrad = True
@@ -28,6 +67,7 @@ class Office(object):
 			manStrat = False
 		if self.trader:
 			self.trader.tick()
+			self.review()
 			manTrad = False
 		if manStrat or manTrad:
 			for manager in self.managers:
