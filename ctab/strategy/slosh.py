e3d24ab0b98b60da688f6a6f422930f4ef6af174
bubbleboy14 <mario.balibrera@gmail.com>
2023-11-11 16:46:16 -0800

    Slosh strategy: swap() and hilo(); tick() uses hilo()

--- ctab/strategy/slosh.py
+++ ctab/strategy/slosh.py
@@ -22,6 +22,47 @@ class Slosh(Base):
 		rats = (collection or self.allratios)[:limit]
 		return sum(rats) / len(rats)
 
+	def swap(self, buysym, sellsym, size=10):
+		hz = self.histories
+		bcur = hz[buysym]["current"]
+		scur = hz[sellsym]["current"]
+		sellsize = size * scur / bcur
+		self.recommender({
+			"action": "SELL",
+			"symbol": sellsym,
+			"price": scur,
+			"size": sellsize
+		})
+		self.recommender({
+			"action": "BUY",
+			"symbol": buysym,
+			"price": bcur,
+			"size": size
+		})
+
+	def hilo(self, cur):
+		size = 0
+		rz = self.ratios
+		az = self.averages
+		rz["current"] = cur
+		if cur > rz["high"]:
+			rz["high"] = cur
+			size += 10
+		elif cur < rz["low"]:
+			rz["low"] = cur
+			size -= 10
+		for ave in ["inner", "outer", "total"]:
+			if cur > az[ave]:
+				size += 1
+		for ave in ["inner", "outer", "total"]:
+			if cur < az[ave]:
+				size -= 1
+		if not size: return
+		if size > 0:
+			self.swap(self.top, self.bottom, size)
+		else:
+			self.swap(self.bottom, self.top, size)
+
 	def tick(self, history=None): # calc ratios (ignore history...)
 		history = self.histories
 		if not self.shouldUpdate:
@@ -35,11 +76,9 @@ class Slosh(Base):
 		self.averages["inner"] = self.ave(INNER)
 		self.averages["outer"] = self.ave(OUTER)
 		if self.ratios["current"]:
-			self.ratios["high"] = max(self.ratios["high"], cur)
-			self.ratios["low"] = min(self.ratios["low"], cur)
+			self.hilo(cur)
 		else:
-			self.ratios["high"] = self.ratios["low"] = cur
-		self.ratios["current"] = cur
+			self.ratios["current"] = self.ratios["high"] = self.ratios["low"] = cur
 		self.log("\n\n", self.ratios, "\n", self.averages, "\n\n")
 
 	def compare(self, symbol, side, price, eobj, history):
