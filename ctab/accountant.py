d84c6de239ef6cef93b1a2b9cc4e3226e2d74985
bubbleboy14 <mario.balibrera@gmail.com>
2023-11-29 05:10:35 -0800

    Accountant: listen(clientReady)->load(); log(), load(), update()

--- ctab/accountant.py
+++ ctab/accountant.py
@@ -1,11 +1,25 @@
+from dydx3.helpers.request_helpers import generate_now_iso
 from backend import log, feed, listen
+from pprint import pprint
 
 class Accountant(object):
-	def __init__(self, platform):
-		# TODO : establish feed ... signature?
-		#ws = feed(platform, ...)
+	def __init__(self):
+		self.ws = None
+		listen("clientReady", self.load)
 		listen("affordable", self.affordable)
 
+	def log(self, *msg):
+		log("Accountant %s"%(" ".join([str(m) for m in msg]),))
+
+	def load(self):
+		if self.ws: return self.log("already loaded")
+		self.log("loading!")
+		self.ws = feed("dacc", generate_now_iso(),
+			on_message=self.update)
+
+	def update(self, msg):
+		pprint(msg)
+
 	def affordable(self, proposal):
-		log("Accountant just saying yes")
+		self.log("just saying yes")
 		return True
