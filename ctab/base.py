6030a815d7dbb22233540643d9805c6f3ca825d6
bubbleboy14 <mario.balibrera@gmail.com>
2023-11-29 15:19:50 -0800

    base: Feeder.feed()

--- ctab/base.py
+++ ctab/base.py
@@ -1,5 +1,5 @@
 from pprint import pprint
-from backend import log, stop
+from backend import log, stop, feed
 
 class Worker(object):
 	def sig(self):
@@ -13,6 +13,14 @@ class Worker(object):
 #		stop()
 
 class Feeder(Worker):
+	def feed(self, platform, channel):
+		self.log("feed", platform, channel)
+		if hasattr(self, "ws"):
+			return self.log("feed already loaded!")
+		self.ws = feed(platform, channel,
+			on_message=self.on_message, on_error=self.on_error,
+			on_open=self.on_open, on_close=self.on_close)
+
 	def on_error(self, ws, msg):
 		self.error(msg)
 
