77ee3768c6121b20e111f4097063db4889e7060f
bubbleboy14 <mario.balibrera@gmail.com>
2023-12-02 02:05:38 -0800

    test.get() uses Agent.credHead()

--- ctab/test.py
+++ ctab/test.py
@@ -5,16 +5,10 @@ from backend import start, spew
 
 def get(path, ag=None):
 	from dez.http import fetch
-	hz = {}
 	ag = ag or Agent()
-	ak = ag.apiCreds()
-	hz["DYDX-API-KEY"] = ak["key"]
-	hz["DYDX-PASSPHRASE"] = ak["passphrase"]
-	ts = hz["DYDX-TIMESTAMP"] = generate_now_iso()
-	hz["DYDX-SIGNATURE"] = ag.signature("/v3/accounts", ts)
+	hz = ag.credHead()
 	host = constants.API_HOST_GOERLI.split("//")[1]
 	print("fetching:", host, path)
-	print("headers:", hz)
 	fetch(host, path, port=443, secure=True, headers=hz, cb=spew, dispatch=True)
 
 def accountant(): # doesn't work - "Invalid id"
