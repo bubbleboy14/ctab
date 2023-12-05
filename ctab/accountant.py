8af78eca90e11b48992cf1ea1ae62b82ddd8fbf9
bubbleboy14 <mario.balibrera@gmail.com>
2023-12-05 01:55:41 -0800

    Accountant: basic accounting (in affordable()); balance{} initialization (in constructor); defbals{}; (need actual balance extraction)

--- ctab/accountant.py
+++ ctab/accountant.py
@@ -2,13 +2,37 @@ from dydx3.helpers.request_helpers import generate_now_iso
 from backend import listen
 from base import Feeder
 
+defbals = {
+	"USD": 1000,
+	"ETH": 0,
+	"BTC": 0
+}
+
 class Accountant(Feeder):
-	def __init__(self):
+	def __init__(self, balances=defbals):
+		self.balances = balances
 		listen("clientReady", self.load)
 		listen("affordable", self.affordable)
 
-	def affordable(self, proposal):
-		self.log("just saying yes")
+	def affordable(self, prop):
+		s = prop.get("size", 10)
+		v = s / prop["price"]
+		bz = self.balances
+		sym = prop["symbol"].split("-")[0]
+		self.log("balances", bz)
+		if prop["action"] == "BUY":
+			if s > bz["USD"]:
+				self.log("not enough USD!")
+				return False
+			bz["USD"] -= s
+			bz[sym] += v
+		else:
+			if s > bz[sym]:
+				self.log("not enough %s!"%(sym,))
+				return False
+			bz["USD"] += s
+			bz[sym] -= v
+		self.log("trade approved!")
 		return True
 
 	def load(self):
