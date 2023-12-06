0402d46afa394e1b0b094e423ef78e878b096b5d
bubbleboy14 <mario.balibrera@gmail.com>
2023-12-06 00:29:13 -0800

    test: get() passes path to credHead(); reg()

--- ctab/test.py
+++ ctab/test.py
@@ -6,7 +6,7 @@ from backend import start, spew
 def get(path, ag=None):
 	from dez.http import fetch
 	ag = ag or Agent()
-	hz = ag.credHead()
+	hz = ag.credHead(path)
 	host = constants.API_HOST_GOERLI.split("//")[1]
 	print("fetching:", host, path)
 	fetch(host, path, port=443, secure=True, headers=hz, cb=spew, dispatch=True)
@@ -16,14 +16,18 @@ def accountant(): # doesn't work - "Invalid id"
 	ag = Agent()
 	start()
 
-def accountById(): # doesn't work - "Unauthorized"
+def accountById(): # works!
 	ag = Agent()
 	get("/v3/accounts/" + ag.id() + "?ethereumAddress=" + ag.client.default_address, ag)
 
-def accounts(): # works
+def accounts(): # works!
 	get("/v3/accounts")
+
+def reg(): # works!
+	get("/v3/registration")
 
 if __name__ == "__main__":
 	accountant()
 	#accountById()
 	#accounts()
+	#reg()
