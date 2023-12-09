7e66ef185a2bbfbcf4068d3d4a4c51e18341984f
bubbleboy14 <mario.balibrera@gmail.com>
2023-12-08 18:12:54 -0800

    init config

--- ctab/init.py
+++ ctab/init.py
@@ -1,1 +1,19 @@
-# TODO
+syms = {
+    ".": ["_ab.py"],
+    "js": ["ab"],
+    "css": ["ab.css"],
+    "html": ["ab"],
+    "bots": ["swapmon.py"]
+}
+model = {
+    "ctab.model": ["*"]
+}
+routes = {
+    "/_ab": "_ab.py"
+}
+cfg = {
+	"mon": {
+		"interval": 1
+	}
+}
+requires = ["ctuser"]
