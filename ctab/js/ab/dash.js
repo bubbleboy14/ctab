b6e5b6feb6bb24dde3b3765f3630bfabd14e8d02
bubbleboy14 <mario.balibrera@gmail.com>
2023-12-08 19:01:07 -0800

    ab.dash.Dash: basic load(), skeletonized build() (now called from constructor), and log()gy update()

--- ctab/js/ab/dash.js
+++ ctab/js/ab/dash.js
@@ -7,17 +7,26 @@ ab.dash = {
 
 ab.dash.Dash = CT.Class({
 	CLASSNAME: "ab.dash.Dash",
-	load: function() {
+	build: function() {
 		CT.dom.setMain([
 
-
+			"[dash]"
 
 		]);
+	},
+	update: function(message) {
+		this.log(message);
+	},
+	load: function() {
+		CT.pubsub.connect(location.hostname, this.opts.port);
+		CT.pubsub.set_cb("message", this.update);
+		CT.pubsub.subscribe("swapmon");
 	},
 	init: function(opts) {
 		this.opts = opts = CT.merge(opts, {
 
 		});
 		this.load();
+		this.build();
 	}
 })
