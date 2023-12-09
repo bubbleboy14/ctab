c102726c1fcf73179346868860ece226fe8c2b0f
bubbleboy14 <mario.balibrera@gmail.com>
2023-12-08 21:50:09 -0800

    SwapMon._tick() returns True (for resched)

--- ctab/bots/swapmon.py
+++ ctab/bots/swapmon.py
@@ -9,10 +9,9 @@ class SwapMon(pubsub.Bot):
 	def __init__(self, server, channel, name="SwapMon"): # only one SwapMon..
 		pubsub.Bot.__init__(self, server, channel, name)
 		rel.timeout(config.ctab.mon.interval, self._tick)
-		log("SwapMon initialized!")
 
 	def _tick(self):
-		log("SwapMon tick!")
 		data = fetch(wcfg.host, "/_ab", wcfg.port, protocol=wcfg.protocol, ctjson=True)
 		log(data)
 		self.pub(data)
+		return True
