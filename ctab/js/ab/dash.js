24bdc408c61a5fc7917b454a9aa1b7ad5164d051
bubbleboy14 <mario.balibrera@gmail.com>
2023-12-08 21:16:22 -0800

    dash legend

--- ctab/js/ab/dash.js
+++ ctab/js/ab/dash.js
@@ -7,15 +7,41 @@ ab.dash = {
 
 ab.dash.Dash = CT.Class({
 	CLASSNAME: "ab.dash.Dash",
+	_: {
+		nodes: {
+			charts: CT.dom.div(),
+			legend: CT.dom.div()
+		},
+		charts: function(data) {
+
+		},
+		leg: function(data) {
+			var _ = this._;
+			return Object.keys(data).map(function(d) {
+				if (isNaN(data[d]))
+					return [d, _.leg(data[d])];
+				return d + ": " + data[d];
+			});
+		},
+		legend: function(data) {
+			var _ = this._;
+			CT.dom.setContent(_.nodes.legend, [
+				CT.dom.div("legend", "big"),
+				_.leg(data)
+			]);
+		}
+	},
 	build: function() {
 		CT.dom.setMain([
-
-			"[dash]"
-
+			CT.dom.div("dash", "bigger"),
+			this._.nodes.legend
+			this._.nodes.charts
 		]);
 	},
 	update: function(message) {
 		this.log(message);
+		this._.legend(message.data);
+		this._.charts(message.data);
 	},
 	load: function() {
 		CT.pubsub.connect(location.hostname, this.opts.port);
