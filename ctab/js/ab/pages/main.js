ca9eb0d9e6370e384fda9ebfe28e0a6e9e0a4467
bubbleboy14 <mario.balibrera@gmail.com>
2023-12-08 23:48:11 -0800

    main page loader: chartist JS/CSS imports

--- ctab/js/ab/pages/main.js
+++ ctab/js/ab/pages/main.js
@@ -2,6 +2,8 @@ CT.require("CT.all");
 CT.require("core");
 CT.require("user.core");
 CT.require("ab.dash");
+CT.scriptImport("https://cdn.jsdelivr.net/chartist.js/latest/chartist.min.js");
+CT.dom.addStyle(null, "https://cdn.jsdelivr.net/chartist.js/latest/chartist.min.css");
 
 CT.onload(function() {
         CT.initCore();
