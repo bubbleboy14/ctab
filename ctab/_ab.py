43dfcb1a8b82f3d6af8d8ee88ff9fed8b0de9c50
bubbleboy14 <mario.balibrera@gmail.com>
2023-12-08 18:38:12 -0800

    basic /_ab (office status())

--- ctab/_ab.py
+++ ctab/_ab.py
@@ -1,1 +1,10 @@
-# TODO
+from mkswap import presets, Office
+from cantools.web import respond, succeed
+from cantools import config
+
+office = Office(**presets[int(config.ctab.office)])
+
+def response():
+	succeed(office.status())
+
+respond(response)
