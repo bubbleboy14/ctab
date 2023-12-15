8819477f9bdc0b34005d0019380eb3b7f4d837d8
bubbleboy14 <mario.balibrera@gmail.com>
2023-12-14 19:48:48 -0800

    swapper (office initializer) passes tcfg.live/staging to setLive()/setStaging()

--- ctab/swapper.py
+++ ctab/swapper.py
@@ -1,11 +1,20 @@
 from mkswap import presets, getOffice
+from mkswap.backend import setStaging
+from mkswap.comptroller import setLive
 from cantools.web import respond
 from cantools.util import log
 from cantools import config
 
+tcfg = config.ctab
+for flag in ["live", "staging"]:
+	if tcfg[flag] == "False":
+		tcfg.update(flag, False)
+
 def response():
 	log("initializing mkswap office", important=True)
-	config.update("office", getOffice(**presets[int(config.ctab.office)]))
+	setLive(tcfg.live)
+	setStaging(tcfg.staging)
+	config.update("office", getOffice(**presets[int(tcfg.office)]))
 	log("office initialized")
 
 respond(response)
