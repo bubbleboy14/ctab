68e34a79148775fd3557b9d75923457f872c0579
bubbleboy14 <mario.balibrera@gmail.com>
2023-12-15 00:40:06 -0800

    _ab curconf and setconf actions (via mkswap.config)

--- ctab/_ab.py
+++ ctab/_ab.py
@@ -1,7 +1,15 @@
-from cantools.web import respond, succeed
+from mkswap.config import config as swapconf
+from cantools.web import respond, succeed, cgi_get
 from cantools import config
 
 def response():
-	succeed(config.office.status())
+	action = cgi_get("action", choices=["status",
+		"curconf", "setconf"], default="status")
+	if action == "curconf":
+		succeed(swapconf.current())
+	elif action == "setconf":
+		swapconf.set(cgi_get("mod"))
+	else: # status (pubsub swapmon)
+		succeed(config.office.status())
 
 respond(response)
