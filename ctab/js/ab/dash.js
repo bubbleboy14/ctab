4df462c9cf82b76fe0b21b6f9ccbe37477917358
bubbleboy14 <mario.balibrera@gmail.com>
2024-03-20 13:27:52 -0700

    small sym/met tab()s

--- ctab/js/ab/dash.js
+++ ctab/js/ab/dash.js
@@ -296,7 +296,7 @@ ab.dash.Dash = CT.Class({
 				CT.dom.flex([
 					_.tab(data, "symbol"),
 					_.tab(data, "metric", "ndx")
-				], "bordered row jcbetween"),
+				], "small bordered row jcbetween"),
 				_.tab(data, "market")
 			], "bigish");
 			wavs = nz["weighted averages"] = CT.dom.div([
