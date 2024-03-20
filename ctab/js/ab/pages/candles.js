be75f596b6510045919e194f4efe069b6ec3a270
bubbleboy14 <mario.balibrera@gmail.com>
2024-03-20 16:31:35 -0700

    candles page loader

---
+++ ctab/js/ab/pages/candles.js
@@ -0,0 +1,10 @@
+CT.require("CT.all");
+CT.require("core");
+CT.require("user.core");
+CT.require("ab.candles");
+CT.scriptImport("https://cdn.jsdelivr.net/npm/apexcharts");
+
+CT.onload(function() {
+        CT.initCore();
+        ab.candles.init();
+});
