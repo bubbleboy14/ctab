835cef3acbf5223c26b19bcad908f244c92293fe
bubbleboy14 <mario.balibrera@gmail.com>
2024-03-20 17:13:45 -0700

    semi-working candles module

--- ctab/js/ab/candles.js
+++ ctab/js/ab/candles.js
@@ -1,30 +1,49 @@
 ab.candles = {
-	_: {},
 	tranCan: function(can) {
 		return {
 			x: new Date(can.timestamp),
 			y: [can.open, can.high, can.low, can.close]
 		};
 	},
+	chart: function(sym, candles) {
+		const n = CT.dom.div();
+		n.build = function() {
+			n.chart = new ApexCharts(n, {
+				xaxis: {
+					type: "datetime"
+				},
+				chart: {
+					height: 350,
+					type: "line"
+				},
+				series: [{
+					name: sym,
+					type: "candlestick",
+					data: candles.map(ab.candles.tranCan)
+				}]
+			});
+			n.chart.render();
+		};
+		return n;
+	},
 	update: function(data) {
-		const chart = new ApexCharts(CT.dom.id("ctmain"), {
-			series: [{
-				name: "candle",
-				type: "candlestick",
-				data: data.candles.map(ab.candles.tranCan)
-			}]
-		});
-		chart.render();
+		const abc = ab.candles, cans = data.message.candles;
+		if (!cans)
+			return CT.dom.setMain("waiting for candles");
+		const cnodes = Object.keys(cans).map(sym => abc.chart(sym, cans[sym]));
+		CT.dom.setMain(cnodes);
+		cnodes.forEach(c => c.build());
 	},
 	start: function() {
 		CT.pubsub.set_autohistory(true);
-		CT.pubsub.connect(location.hostname, this.opts.port);
-		CT.pubsub.set_cb("message", this.update);
+		CT.pubsub.connect(location.hostname, core.config.ctab.dash.port);
+		CT.pubsub.set_cb("message", ab.candles.update);
 		CT.pubsub.subscribe("swapmon");
 	},
 	init: function(opts) {
 		const abc = ab.candles;
-		abc._.opts = opts = CT.merge(opts, {
+		abc.log = CT.log.getLogger("candles");
+		abc.opts = opts = CT.merge(opts, {
 			startWS: true
 		});
 		opts.startWS && abc.start();
