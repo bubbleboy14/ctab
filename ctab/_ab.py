8e727b401032ce69dadaeb7007c4c5d60f6cb425
bubbleboy14 <mario.balibrera@gmail.com>
2024-03-22 23:23:56 -0700

    /_ab bt

--- ctab/_ab.py
+++ ctab/_ab.py
@@ -1,11 +1,13 @@
+from rel.util import emit
 from mkswap.config import config as swapconf
 from cantools.web import respond, succeed, cgi_get
+from cantools.util import log
 from cantools import config
 
 def response():
 	office = config.ctab.live.office
 	action = cgi_get("action", choices=["status", "curconf",
-		"setconf", "cancel", "candles"], default="status")
+		"setconf", "cancel", "candles", "bt"], default="status")
 	if action == "cancel":
 		token = cgi_get("token")
 		if token == "all":
@@ -18,6 +20,11 @@ def response():
 		swapconf.set(cgi_get("mod"))
 	elif action == "candles":
 		succeed(office and office.actuary.oldCandles() or { "waiting": "candles loading" })
+	elif action == "bt":
+		sym = cgi_get("sym")
+		side = cgi_get("side")
+		log("/_ab forwarding %s %s order"%(sym, side))
+		emit("balTrades", sym, side)
 	else: # status (pubsub swapmon)
 		succeed(office and office.status() or { "waiting": "office loading" })
 
