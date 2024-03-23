8c59c3fe1a3e843bf31cb8eb2042bccd0db700eb
bubbleboy14 <mario.balibrera@gmail.com>
2024-03-22 20:15:33 -0700

    dash page loader: ab.candles require() and apex scriptImport()

--- ctab/js/ab/pages/main.js
+++ ctab/js/ab/pages/main.js
@@ -2,7 +2,9 @@ CT.require("CT.all");
 CT.require("core");
 CT.require("user.core");
 CT.require("ab.util");
+CT.require("ab.candles");
 CT.require("ab.dash");
+CT.scriptImport("https://cdn.jsdelivr.net/npm/apexcharts");
 CT.scriptImport("https://cdn.jsdelivr.net/chartist.js/latest/chartist.min.js");
 CT.dom.addStyle(null, "https://cdn.jsdelivr.net/chartist.js/latest/chartist.min.css");
 
