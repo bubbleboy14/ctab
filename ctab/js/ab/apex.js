daaf16dfc39d4ca187c09640cd9688872796add0
bubbleboy14 <mario.balibrera@gmail.com>
2024-04-05 13:30:33 -0700

    moved apex scriptImport() from candles loader to apex loader

--- ctab/js/ab/apex.js
+++ ctab/js/ab/apex.js
@@ -1,2 +1,3 @@
 CT.require("ab.apex.trans");
 CT.require("ab.apex.Graph");
+CT.scriptImport("https://cdn.jsdelivr.net/npm/apexcharts");
--- ctab/js/ab/candles.js
+++ ctab/js/ab/candles.js
@@ -2,7 +2,6 @@ CT.require("ab.apex");
 CT.require("ab.candles.util");
 CT.require("ab.candles.latest");
 CT.require("ab.candles.Manager");
-CT.scriptImport("https://cdn.jsdelivr.net/npm/apexcharts");
 
 ab.candles.init = function(opts) {
 	const abc = ab.candles;
