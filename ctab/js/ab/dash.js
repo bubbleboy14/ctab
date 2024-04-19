42ec58d7805150b384a778d06f22a0458cf5ef10
bubbleboy14 <mario.balibrera@gmail.com>
2024-04-19 14:35:29 -0700

    smaller marchar

--- ctab/js/ab/dash.js
+++ ctab/js/ab/dash.js
@@ -320,7 +320,7 @@ ab.dash.Dash = CT.Class({
 			CT.dom.setContent(_.nodes.prices, [
 				leggy,
 				symet,
-				_.tab(data, "market", null, "fs70p")
+				_.tab(data, "market", null, "marchar")
 			], "bigish");
 			wavs = nz["weighted averages"] = CT.dom.div([
 				_.leg({ "asks": data.weighted.ask }, false, null, true),
