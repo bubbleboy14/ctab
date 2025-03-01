a20d6627addc19a035594ed70e0c46060fc4dc4d
bubbleboy14 <mario.balibrera@gmail.com>
2025-02-28 23:33:32 -0800

    tpv fullbals

--- ctab/js/ab/tpv.js
+++ ctab/js/ab/tpv.js
@@ -1,14 +1,55 @@
 ab.tpv = {
 	_: {
+		bals: ["total", "usd", "eth", "btc"],
+		secs: ["prices", "balances"],
+		gropts: {
+			sym: "TPV",
+			name: "total",
+			height: "85%"
+		},
+		graph: function() {
+			const _ = ab.tpv._;
+			new ab.apex.Graph(CT.merge({
+				items: _.tpvs,
+				graphopts: {
+					tooltip: {
+						custom: _.tooltip
+					}
+				}
+			}, _.gropts));
+		},
+		baller: function(d) {
+			let sym, slo;
+			d.usdbals = {};
+			for (sym in d.balances) {
+				slo = sym.toLowerCase();
+				d[slo] = d.balances[sym];
+				if (sym != "USD") {
+					d[slo] *= d.prices[sym + "USD"];
+					d.usdbals[sym] = d[slo];
+				}
+			}
+		},
+		balbutt: function() {
+			const _ = ab.tpv._, b = CT.dom.button("show all balances", function() {
+				_.tpvs.forEach(_.baller);
+				_.gropts.parts = _.bals;
+				_.secs.push("usdbals");
+				CT.dom.clear("ctmain");
+				b.remove();
+				_.graph();
+			});
+			return b;
+		},
 		kvblock: function(obj, key) {
 			const lines = [key + ":"], d = obj[key];
 			for (let k in d)
 				lines.push("<span>" + k + ": <b>" + d[k] + "</b></span>");
-			return lines.join("") + "<br>";
+			return lines.join("");
 		},
 		dstring: function(d) {
 			const _ = ab.tpv._;
-			return _.kvblock(d, "prices") + _.kvblock(d, "balances") + "total: " + d.total;
+			return _.secs.map(s => _.kvblock(d, s)).concat(["total: " + d.total]).join("<br>");
 		},
 		tooltip: function({series, seriesIndex, dataPointIndex, w}) {
 			const _ = ab.tpv._, ttt = w.globals.tooltip.tooltipTitle;
@@ -18,19 +59,9 @@ ab.tpv = {
 	build: function(tpvs) {
 		const _ = ab.tpv._;
 		_.tpvs = tpvs;
-		this.graph = new ab.apex.Graph({
-			sym: "TPV",
-			items: tpvs,
-			name: "total",
-			height: "85%",
-			graphopts: {
-				tooltip: {
-					custom: _.tooltip
-				}
-			}
-		});
+		_.graph();
 	},
 	init: function(opts) {
-		ab.popper.build("tpv", ab.tpv.build);
+		ab.popper.build("tpv", ab.tpv.build, ab.tpv._.balbutt());
 	}
 }
