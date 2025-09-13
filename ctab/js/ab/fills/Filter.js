66244d8e939749c28f599f3488367f34997da60b
bubbleboy14 <mario.balibrera@gmail.com>
2025-09-12 23:15:29 -0700

    score filtering

--- ctab/js/ab/fills/Filter.js
+++ ctab/js/ab/fills/Filter.js
@@ -1,22 +1,94 @@
 ab.fills.Filter = CT.Class({
 	CLASSNAME: "ab.fills.Filter",
 	_: {
-		reasons: ["all"],
-		ratbutt: function() {
+		reasons: [],
+		reason: function() {
 			const _ = this._, oz = this.opts;
-			return CT.dom.button("reason filter", function() {
-				CT.modal.choice({
-					prompt: "which reason?",
-					data: _.reasons,
-					cb: function(sel) {
-						oz.fills = oz.allfills;
-						if (sel != "all")
-							oz.fills = oz.fills.filter(f => f.rationale && f.rationale.reason == sel);
-						CT.dom.clear("ctmain");
-						oz.refresher();
-					}
-				});
+			CT.modal.choice({
+				prompt: "what reason?",
+				data: _.reasons,
+				cb: function(sel) {
+					oz.fills = oz.fills.filter(f => f.rationale && f.rationale.reason == sel);
+					_.refresh("reason");
+				}
 			});
+		},
+		score: function() {
+			const fs = this.score;
+			fs.data || fs.calc();
+			fs.prompt();
+		},
+		clear: function() {
+			this.opts.fills = this.opts.allfills;
+			this._.refresh();
+		},
+		refresh: function(filtered) {
+			this._.filtered = filtered
+			CT.dom.clear("ctmain");
+			this.opts.refresher();
+		}
+	},
+	score: {
+		calc: function() {
+			const data = this.score.data = {}, prices = this.opts.prices;
+			let score, pold, pnew, vold, vnew;
+			this.log("scoring", prices);
+			CT.net.spinOn();
+			this.opts.allfills.forEach(function(fill) {
+				// market, amount, price, side, fee
+				pnew = prices[fill.market];
+				pold = fill.price;
+				amount = fill.amount;
+				vnew = amount * pnew;
+				vold = amount * pold;
+				if (fill.side == "buy")
+					score = vnew - vold;
+				else // sell
+					score = vold - vnew;
+				score -= fill.fee; // right?
+				if ("total" in data) {
+					data.total += score;
+					data.min = Math.min(score, data.min);
+					data.max = Math.max(score, data.max);
+				} else
+					data.total = data.min = data.max = score;
+				fill.score = {
+					prospective: fill.score,
+					retrospective: score
+				}
+			});
+			CT.net.spinOff();
+			this.log("scored", data);
+		},
+		filt: function(direction, num) {
+			if (direction == "above")
+				return f => f.score.retrospective > num;
+			return f => f.score.retrospective < num;
+		},
+		select: function(direction) {
+			const _ = this._, oz = this.opts, s = this.score, data = s.data;
+			CT.modal.prompt({
+				prompt: "what cutoff?",
+				style: "number",
+				min: data.min,
+				max: data.max,
+				unit: 0.01,
+				cb: function(num) {
+					oz.fills = oz.fills.filter(s.filt(direction, num));
+					_.refresh("score");
+				}
+			})
+		},
+		prompt: function() {
+			CT.modal.choice({
+				prompt: "what direction?",
+				data: ["above", "below"],
+				cb: this.score.select
+			});
+		},
+		prices: function(prices) {
+			this.opts.prices = prices;
+			this.log("prices", prices);
 		}
 	},
 	balanced: function(f) {
@@ -27,9 +99,23 @@ ab.fills.Filter = CT.Class({
 		return true;
 	},
 	filters: function() {
-		return this._.ratbutt();
+		const _ = this._;
+		let filts;
+		return CT.dom.button("filter", function() {
+			filts = ["reason", "score"];
+			if (_.filtered) {
+				CT.data.remove(filts, _.filtered);
+				filts.push("clear");
+			}
+			CT.modal.choice({
+				prompt: "what filter?",
+				data: filts,
+				cb: filt => _[filt]()
+			})
+		});
 	},
 	init: function(opts) {
 		this.opts = opts;
+		ab.util.req(this.score.prices, "prices");
 	}
 });
